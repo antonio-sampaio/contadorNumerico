da0c12374e2d0b9aa14d1879c7984db19e7ee452
antonio-sampaio <andre-s.sampaio@hotmail.com>
2022-11-11 12:12:28 -0300

    1 commit do arquivo contador

---
+++ script.js
@@ -0,0 +1,21 @@
+const btnIncrementar$ = document.getElementById("btn_incrementar");
+const btnDecrementar$ = document.getElementById("btn_decrementar");
+const btnZerar$ = document.getElementById("btn_zerar");
+const p$ = document.getElementById("contador");
+
+let contador = 0;
+p$.innerHTML = contador;
+
+btnIncrementar$.addEventListener("click", function(){
+    //contador++;
+    p$.innerHTML = ++contador;
+});
+btnDecrementar$.addEventListener("click", () => {
+    p$.innerHTML = --contador;
+})
+btnZerar$.addEventListener("click",() => {
+    p$.innerHTML = contador = 0;
+})
+
+console.log(btnIncrementar$);
+console.log(p$);
